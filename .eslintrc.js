52371d6d168c3241e6f27fcb4e75fd2e8ce7e88d
kn3ny <30493783+kn3ny@users.noreply.github.com>
2021-02-21 14:03:53 +0900

    eslintとprettierを共存

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
         "node": true,
     },
     "extends": [
-        "eslint:recommended"
+        "eslint:recommended",
+        "prettier",
     ],
     "parserOptions": {
         "ecmaVersion": 12,
